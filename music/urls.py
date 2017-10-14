7ca2d7ebc158cd82c8fc7ff3c6fb6d3290def050
unknown <sbginter@gmail.com>
2017-10-14 21:10:33 +0300

    adding template to the music app

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    # / Music /
+    url(r'^$', views.index, name='index'),
+
+    # /Music/[id]
+
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail')
 ]
--- music/views.py
+++ music/views.py
@@ -1,4 +1,18 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Album
 
 def index(request):
-    return HttpResponse('<h1>this is a music project</h1>')
+    all_albums = Album.objects.all()
+    # html = ''
+    # for album in all_albums:
+    #     url = '/music/' +  str(album.id) + '/'
+    #     html += '<a href="' + url + '">' + album.album_title + ' </a><br> '
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums' : all_albums,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, album_id):
+        return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
