c36252d6690c6eb2b0c3cc0837f9fffde2289894
unknown <sbginter@gmail.com>
2017-10-14 21:16:19 +0300

    adding shortcut to the code to render data effectively

--- music/views.py
+++ music/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.template import loader
+# from django.template import loader
+from django.shortcuts import render
 from .models import Album
 
 def index(request):
@@ -8,11 +9,10 @@ def index(request):
     # for album in all_albums:
     #     url = '/music/' +  str(album.id) + '/'
     #     html += '<a href="' + url + '">' + album.album_title + ' </a><br> '
-    template = loader.get_template('music/index.html')
-    context = {
-        'all_albums' : all_albums,
-    }
-    return HttpResponse(template.render(context, request))
+    # template = loader.get_template('music/index.html')
+    context = {'all_albums' : all_albums}
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
         return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
