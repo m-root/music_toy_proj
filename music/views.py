0ff6f5a2863b3ab2ef48076001a0377e282d0d33
unknown <sbginter@gmail.com>
2017-10-15 10:16:16 +0300

    added views and models. also added music to the db and plugged in music in the admin side

--- music/views.py
+++ music/views.py
@@ -1,18 +1,17 @@
-from django.http import HttpResponse
-# from django.template import loader
+from django.http import Http404
 from django.shortcuts import render
 from .models import Album
 
+#Indexing Music
 def index(request):
     all_albums = Album.objects.all()
-    # html = ''
-    # for album in all_albums:
-    #     url = '/music/' +  str(album.id) + '/'
-    #     html += '<a href="' + url + '">' + album.album_title + ' </a><br> '
-    # template = loader.get_template('music/index.html')
     context = {'all_albums' : all_albums}
-    # return HttpResponse(template.render(context, request))
     return render(request, 'music/index.html', context)
 
+# Try checking if there is an album with the ID and if not it returns an error response
 def detail(request, album_id):
-        return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'albums' : album})
