6684f22841058d21c03116a8a97fd35e59455642
unknown <sbginter@gmail.com>
2017-10-22 18:23:30 +0300

    Album CURD

--- music/urls.py
+++ music/urls.py
@@ -7,7 +7,13 @@ urlpatterns = [
     # / Music /
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<album_id>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    # Music/Album/Add
-    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add')
 
+    # Music/Album/Add
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    # Music/Album/#/update
+    url(r'?P<album_id>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-updae'),
+
+    # Music/Album/delete
+    url(r'?P<album_id>[0-9]+)/delete/$', views.AlbumCreate.as_view(), name='album-delete')
 ]
