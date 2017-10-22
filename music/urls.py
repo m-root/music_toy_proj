052914de8b753c4f3d7d94b1634d8aa195b961ce
unknown <sbginter@gmail.com>
2017-10-22 17:19:55 +0300

    Addition of form template

--- music/urls.py
+++ music/urls.py
@@ -5,12 +5,9 @@ app_name = 'music'
 
 urlpatterns = [
     # / Music /
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<album_id>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    # Music/Album/Add
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add')
 
-    # /Music/[id]
-
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
-
-    # /Music/Album_[id]/favourite/
-    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name='favourite'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,27 +1,21 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Album, Song
+
+from django.views import generic
+from django.views.generic import CreateView, UpdateView, DeleteView
+from .models import Album
 
 
-#Indexing Music
-def index(request):
-    all_albums = Album.objects.all()
-    context = {'all_albums' : all_albums}
-    return render(request, 'music/index.html', context)
+class IndexView(generic.ListView):
+    template_name = "music/index.html"
+    context_object_name = 'all_albums'
 
-# Try checking if there is an album with the ID and if not it returns an error response
-def detail(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album' : album})
+    def get_queryset(self):
+        return Album.objects.all()
 
-def favourite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except(KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', { 'album' : album,
-                                                    'error_message' : "You did not select a valid song"
-                                                    })
-    else:
-        selected_song.is_favourite = True
-        selected_song.save()
-        return render(request, 'music/detail.html', {'albums': album})
+
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_log']
