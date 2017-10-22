bde96db92218f44b3b3079641be8c7959bbc5906
unknown <sbginter@gmail.com>
2017-10-22 21:28:18 +0300

    Added image upload functionality and also some user form components

--- music/urls.py
+++ music/urls.py
@@ -6,14 +6,14 @@ app_name = 'music'
 urlpatterns = [
     # / Music /
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<album_id>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
     # Music/Album/Add
-    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+    url(r' album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 
     # Music/Album/#/update
-    url(r'?P<album_id>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-updae'),
+    url(r'^(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-updae'),
 
     # Music/Album/delete
-    url(r'?P<album_id>[0-9]+)/delete/$', views.AlbumCreate.as_view(), name='album-delete')
+    url(r'^(?P<pk>[0-9]+)/delete/$', views.AlbumCreate.as_view(), name='album-delete')
 ]
--- music/views.py
+++ music/views.py
@@ -1,8 +1,12 @@
 
 from django.views import generic
 from django.views.generic import CreateView, UpdateView, DeleteView
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
 from django.core.urlresolvers import reverse_lazy
+from django.views.generic import View
 from .models import Album
+from .forms import UserForm
 
 
 class IndexView(generic.ListView):
